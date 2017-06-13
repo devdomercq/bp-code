a25ecc873c61762227da44b107352b66de8cf7f8
Devon Domercq <djdomercq@gmail.com>
2017-06-13 22:26:48 +0000

    Add New and Create Actions with View Files

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -2,4 +2,19 @@ class ScoreboardsController < ApplicationController
     
     def index
     end
+    
+    def new
+        @scoreboard = Scoreboard.new
+    end
+    
+    def create
+        @scoreboard = Scoreboard.new(scoreboard_params)
+    end
+    
+    private
+    
+    def scoreboard_params
+        params.require(:scoreboard).permit(:bpecode, :organization, :code, :description)
+    end
+    
 end
