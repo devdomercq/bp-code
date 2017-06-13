48d872f27b18b9b7f8145559ce2cda10c139e80a
Devon Domercq <djdomercq@gmail.com>
2017-06-13 22:05:27 +0000

    Scoreboard Model and Controller

---
+++ db/migrate/20170613215759_create_scoreboards.rb
@@ -0,0 +1,12 @@
+class CreateScoreboards < ActiveRecord::Migration
+  def change
+    create_table :scoreboards do |t|
+      t.string :bpecode
+      t.string :organization
+      t.text :code
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
