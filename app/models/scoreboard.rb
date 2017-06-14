cdf13808058d8363485eb782d62932f361e29108
Devon Domercq <djdomercq@gmail.com>
2017-06-14 04:59:57 +0000

    Render images on Index page

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -4,7 +4,7 @@ class Scoreboard < ActiveRecord::Base
     
    
   has_attached_file :scoreboard_img, 
-                    :styles => { :scoreboard_index => "352x198>", :scoreboard_show => "480x270>" }, 
+                    :styles => { :scoreboard_index => "480x270>", :scoreboard_show => "480x270>" }, 
                     :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :scoreboard_img, :content_type => /\Aimage\/.*\z/
 
