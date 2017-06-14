371bead14956823721db82a26db2549ca30a8477
Devon Domercq <djdomercq@gmail.com>
2017-06-14 19:43:13 +0000

    Update controller to fix category/search menus

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -2,18 +2,14 @@ class ScoreboardsController < ApplicationController
     before_action :find_scoreboard, only: [:show, :edit, :update, :destroy]
     
     def index
-        if params[:category].blank?
-            @scoreboards = Scoreboard.all.order("created_at DESC")
-        else
+        if !params[:category].blank?
             @category_id = Category.find_by(name: params[:category]).id
             @scoreboards = Scoreboard.where(:category_id => @category_id).order("created_at DESC")
-        end
-     
-        @scoreboards = Scoreboard.all
-            if params[:search]
-        @scoreboards = Scoreboard.search(params[:search]).order("created_at DESC")
-            else
-        @scoreboards = Scoreboard.all.order("created_at DESC")
+        elsif params[:search]
+            @scoreboards = Scoreboard.search(params[:search]).order("created_at DESC")
+        else
+            @scoreboards = Scoreboard.all.order("created_at DESC")
+
   
         end  
         
