ef98a222b2ce4972594c4d0dd446ac23221f5803
Devon Domercq <djdomercq@gmail.com>
2017-06-14 04:14:10 +0000

    Fixed ImageMagick/Paperclip file uploads

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -1,4 +1,12 @@
 class Scoreboard < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
+    
+   
+  has_attached_file :scoreboard_img, 
+                    :styles => { :scoreboard_index => "352x198>", :scoreboard_show => "480x270>" }, 
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :scoreboard_img, :content_type => /\Aimage\/.*\z/
+
 end
+
