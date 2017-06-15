166445109d4f5797ca0abed313fb0ee16d43be85
Devon Domercq <djdomercq@gmail.com>
2017-06-15 00:52:57 +0000

    Turn on form category requirement

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,6 @@
-Paperclip::Attachment.default_options[:url] = ':s3.us-east-2.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
-Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
+# use s3 for production hosting
+unless Rails.env.development?
+    Paperclip::Attachment.default_options[:url] = ':s3.us-east-2.amazonaws.com'
+    Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
+    Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
+  end
